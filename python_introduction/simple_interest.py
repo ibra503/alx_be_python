2084efcdf33d9a6b99b515b09822ac6611c026f8
ibra503 <aouzalibrahim80@gmail.com>
2025-05-24 20:24:11 +0100

    all the errors have been fixed according to the instructions

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -2,5 +2,5 @@
 Principal = 1000
 Rate = 0.05
 Time = 3
-I = Principal * Rate * Time 
-print ("The simple interest is" , I)
+interest = Principal * Rate * Time 
+print ("The simple interest is" , interest)
