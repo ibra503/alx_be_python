a875f9fd2ec0325d1cd2d1b114ac895bad982977
Ibra503 <aouzalibrahim80@gmail.com>
2025-06-29 19:30:57 +0100

    oop file added

--- oop/library_system.py
+++ oop/library_system.py
@@ -1,9 +1,11 @@
+# library_system.py
+
 class Book:
     def __init__(self, title, author):
         self.title = title
         self.author = author
 
-    def get_details(self):
+    def __str__(self):
         return f"Book: {self.title} by {self.author}"
 
 
@@ -12,7 +14,7 @@ class EBook(Book):
         super().__init__(title, author)
         self.file_size = file_size  # in KB
 
-    def get_details(self):
+    def __str__(self):
         return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
 
 
@@ -21,17 +23,17 @@ class PrintBook(Book):
         super().__init__(title, author)
         self.page_count = page_count
 
-    def get_details(self):
+    def __str__(self):
         return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
 
 
 class Library:
     def __init__(self):
-        self.books = []  
+        self.books = []
 
     def add_book(self, book):
         self.books.append(book)
 
     def list_books(self):
         for book in self.books:
-            print(book.get_details())
+            print(book)  # ✅ Use __str__ of each object
